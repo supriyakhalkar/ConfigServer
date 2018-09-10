8ad515de0cc28aacab9b97d69fa4edb0f5bd4d13
supriyakhalkar <supriya.khalkar@synerzip.com>
2018-09-10 18:38:35 +0530

    config server

--- src/main/java/com/synerzip/training/ConfigServerApplication.java
+++ src/main/java/com/synerzip/training/ConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class B1ConfigServerApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(B1ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
